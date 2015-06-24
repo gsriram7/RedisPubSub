09dc759d2943bddf7dbe7bfe2d60b6f3c3cb36d8
gsriram7@gmail.com <vaishnavi7>
2015-06-24 11:12:20 +0530

    Redis pub sub using logger

---
+++ src/com/company/Subscriber.java
@@ -0,0 +1,40 @@
+package com.company;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.JedisPubSub;
+
+public class Subscriber extends JedisPubSub {
+
+    private static Logger logger = LoggerFactory.getLogger(Subscriber.class);
+
+    @Override
+    public void onMessage(String channel, String message) {
+        System.out.println(message);
+        logger.info("Message received. Channel: {}, Msg: {}", channel, message);
+    }
+
+    @Override
+    public void onPMessage(String pattern, String channel, String message) {
+
+    }
+
+    @Override
+    public void onSubscribe(String channel, int subscribedChannels) {
+
+    }
+
+    @Override
+    public void onUnsubscribe(String channel, int subscribedChannels) {
+
+    }
+
+    @Override
+    public void onPUnsubscribe(String pattern, int subscribedChannels) {
+
+    }
+
+    @Override
+    public void onPSubscribe(String pattern, int subscribedChannels) {
+
+    }
+}
